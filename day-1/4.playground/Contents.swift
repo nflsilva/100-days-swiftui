5d450b9b3645256242f2af2e99a847ac883ee598
nfls <nfls@nflss-iMac-Pro.local>
2023-04-25 23:20:57 +0100

    added day-1

--- day-1/4.playground/Contents.swift
+++ day-1/4.playground/Contents.swift
@@ -1,3 +1,23 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphane"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
--- day-1/5.playground/Contents.swift
+++ day-1/5.playground/Contents.swift
@@ -1,3 +1,22 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let actor = "Denzel Washington"
+let fileName = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+
+let movie = """
+A day in
+the life of an
+Apple engineer.
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("a day"))
+print(fileName.hasSuffix(".jpg"))
