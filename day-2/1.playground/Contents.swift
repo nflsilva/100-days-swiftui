a9fa94d8c176b8c8990142b4971f055551fcc95d
nflsilva <nelsonflsilva@gmail.com>
2023-04-26 23:15:29 +0100

    added day-2.

---
+++ day-2/1.playground/Contents.swift
@@ -0,0 +1,21 @@
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+let goodDogs = true
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+var gameOver = false
+print(gameOver)
+
+gameOver.toggle()
+print(gameOver)
---
+++ day-2/checkpoint-1.playground/Contents.swift
@@ -0,0 +1,5 @@
+
+let celsius = 20.0
+let fahrenheit = celsius * 9 / 5 + 32
+
+print("\(celsius)ºC / \(fahrenheit)ºF")
