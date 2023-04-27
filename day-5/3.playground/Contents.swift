a320110568ffcf67960bc9b09df5b689fe0f8cd9
nflsilva <nelsonflsilva@gmail.com>
2023-04-27 23:51:56 +0100

    added day-5

---
+++ day-5/3.playground/Contents.swift
@@ -0,0 +1,31 @@
+enum Weather {
+    case sun, rain, wind, snow, unknown
+}
+
+let forecast = Weather.sun
+
+if forecast == .sun {
+    print("It should be a nice day.")
+} else if forecast == .rain {
+    print("Pack an umbrella.")
+} else if forecast == .wind {
+    print("Wear something warm")
+} else if forecast == .rain {
+    print("School is cancelled.")
+} else {
+    print("Our forecast generator is broken!")
+}
+
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain:
+    print("Pack an umbrella.")
+case .wind:
+    print("Wear something warm")
+case .snow:
+    print("School is cancelled.")
+    fallthrough
+case .unknown:
+    print("Our forecast generator is broken!")
+}
