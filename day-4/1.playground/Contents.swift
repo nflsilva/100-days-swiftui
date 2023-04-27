2f454cf2dfba2601c970b5f047d0dd2dd811ad86
nflsilva <nelsonflsilva@gmail.com>
2023-04-27 23:31:12 +0100

    added day-4

---
+++ day-4/1.playground/Contents.swift
@@ -0,0 +1,20 @@
+let surname: String = "Lasso"
+var score: Int = 0
+
+let playerName: String = "Roy"
+
+var teams: [String] = [String]()
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style = UIStyle.light
+
+let username: String
+// lots of complex logic
+username = "@twostraws"
+// lots more complex logic
+print(username)
---
+++ day-4/checkpoint-2.playground/Contents.swift
@@ -0,0 +1,5 @@
+
+let chars = ["a", "b", "c", "d", "e", "a", "b"]
+print("Number of items: \(chars.count).")
+let uniqueChars = Set(chars)
+print("Number of unique items: \(uniqueChars.count).")
