f275c8ba1d3eaedb40560c4491df5bce19f9fe90
Nelson Lopes Silva <5671236+nflsilva@users.noreply.github.com>
2023-05-04 16:36:23 +0100

    added day-10

---
+++ day-10/4.playground/Contents.swift
@@ -0,0 +1,12 @@
+struct Player {
+    let name: String
+    let number: Int
+
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+print(player.number)
