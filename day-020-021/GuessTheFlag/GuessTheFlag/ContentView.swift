e7081b829da90ae98b47fdc0b15c738f3638d73a
nflsilva <nelsonflsilva@gmail.com>
2023-05-12 00:11:27 +0100

    finished day-21

--- day-020-021/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ day-020-021/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -13,56 +13,87 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var alertTitle = "Wrong"
+    @State private var alertText = "Wrong"
     @State private var score = 0
     
     var body: some View {
         ZStack {
-
+            
             LinearGradient(gradient: Gradient(colors: [.blue, .black]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
-            .ignoresSafeArea()
+                .ignoresSafeArea()
+                .frame(maxWidth: .infinity)
             
-            VStack(spacing: 30) {
-                Text("Tap the flag of")
+            VStack {
+                Spacer()
+                Text("Guess The Flag")
+                    .font(.largeTitle.bold())
                     .foregroundColor(.white)
                 
-                Text(countries[correctAnswer])
-                    .foregroundColor(.white)
-                    .bold()
-                
-                ForEach(0..<3) { number in
-                    Button {
-                        alertIsPresented = true
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundColor(.black)
+                            .font(.subheadline.weight(.heavy))
                         
+                        Text(countries[correctAnswer])
+                            .foregroundColor(.black)
+                            .font(.largeTitle.weight(.semibold))
                         
-    
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
+                        ForEach(0..<3) { number in
+                            Button {
+                                answearQuestion(number)
+                                alertIsPresented = true
+                            } label: {
+                                Image(countries[number])
+                                    .renderingMode(.original)
+                                    .clipShape(Capsule())
+                                    .shadow(radius: 6)
+                            }
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
             }
+            .padding()
         }
-        .alert("hey", isPresented: $alertIsPresented) {
-            Button("Continue", role: .cancel) { }
+        .alert(alertTitle, isPresented: $alertIsPresented) {
+            Button("Continue", role: .cancel) {
+                askQuestion()
+            }
         } message: {
-            Text("Hello")
+            Text(alertText)
         }
     }
     
-    func askQuestion() {
+    private func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
     
-    func answearQuestion(_ responde: Int) {
+    private func answearQuestion(_ responde: Int) {
         if responde == correctAnswer {
-            alertTitle = "Correct"
             score += 1
+            alertTitle = "Well done!"
+            alertText = "Score:\(score)"
         }
         else {
             alertTitle = "Wrong"
+            alertText = "That was flag number \(correctAnswer)"
         }
     }
     
