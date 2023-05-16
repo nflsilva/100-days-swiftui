cf342c2be631690bfc5e674c0002a9b2eb539670
nflsilva <nelsonflsilva@gmail.com>
2023-05-16 22:55:31 +0100

    added day-026

---
+++ day-026/MLDemo/MLDemo/MLDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLDemoApp.swift
+//  MLDemo
+//
+//  Created by nfls on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MLDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
