661c24db8fc89f13aa60542ee455440fcac6b3c7
nflsilva <nelsonflsilva@gmail.com>
2023-07-23 23:50:38 +0100

    feat: added day-063

---
+++ day-063/ImageApp/ImageApp/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  ImageApp
+//
+//  Created by nfls on 23/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var image: Image?
+    @State private var inputImage: UIImage?
+    @State private var showingPicker = false
+    
+    var body: some View {
+        VStack {
+            
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Select an image") {
+                showingPicker = true
+            }
+        }
+        .sheet(isPresented: $showingPicker) {
+            ImagePicker(image: $inputImage)
+        }
+        .onChange(of: inputImage) { _ in loadImage() }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day-063/ImageApp/ImageApp/ImageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAppApp.swift
+//  ImageApp
+//
+//  Created by nfls on 23/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
